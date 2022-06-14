3c174eff978af2e8ccb74afeb44a69ccf7036a6b
Naoki <naoki@NaokinoMacBook-Air.local>
2022-06-14 21:21:56 +0900

    マイグレーション追加

---
+++ database/migrations/2022_06_14_115917_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 150)->charset('utf8mb4')->collation('utf8mb4_bin');
+            $table->text('content')->charset('utf8mb4')->collation('utf8mb4_bin');
+            $table->dateTime('action_date');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
